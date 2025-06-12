20fca184304225d22c763ea86c0131af77eb5c6f
grottaAzzurra <125438751+grottaAzzurra@users.noreply.github.com>
2025-06-12 01:38:12 +0000

    add second space to header

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        st.write('## ' + fruit_chosen + 'Nutrition Information')
+        st.write('## ' + fruit_chosen + ' Nutrition Information')
         
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
