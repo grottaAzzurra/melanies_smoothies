bd689ea7908ff0ffa52a92799758ede859a31ea4
grottaAzzurra <125438751+grottaAzzurra@users.noreply.github.com>
2025-06-12 01:30:35 +0000

    support for using search on in froot api

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response)
